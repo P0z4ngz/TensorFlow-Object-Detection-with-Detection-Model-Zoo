f3ab51980b0bb0936e68b8e29309b001f220dcab
Fauzan Maulana <fauzanmaulana111713@gmail.com>
2024-10-01 18:01:41 +0800

    Add Comments

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from detection import Detector
 # You can get the pre-trained model on https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/tf2_detection_zoo.md
 MODEL_URL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v1_fpn_640x640_coco17_tpu-8.tar.gz"
 
-IMG_PATH = "pictures/people_happy.jpg"
-VID_PATH = "videos/traffic_highways.mp4"
-CLASS_PATH = "object_class_names.txt"
+IMG_PATH = "pictures/people_happy.jpg"  #customizable
+VID_PATH = "videos/traffic_highways.mp4"    ##customizable
+CLASS_PATH = "object_class_names.txt"   # From COCO dataset
 
 detector = Detector()
 detector.read_classes(CLASS_PATH)
 detector.get_model(MODEL_URL)
 detector.load_model()
 detector.predict_image(image_path=IMG_PATH)
+
+# Comment the detector.predict_image() and Uncomment this to run object detection video
 #detector.predict_video(video_path=VID_PATH)
